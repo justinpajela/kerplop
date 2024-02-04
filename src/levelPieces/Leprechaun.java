177c71b61ead1a93d54aa8c0a45fa3f180d0f321
Brody Perlick <brodyperlick@mines.edu>
2024-02-04 16:17:53 -0700

    Added leprechaun and witch classes

---
+++ src/levelPieces/Leprechaun.java
@@ -0,0 +1,60 @@
+package levelPieces;
+
+import gameEngine.Drawable;
+import gameEngine.InteractionResult;
+import gameEngine.Moveable;
+
+public class Leprechaun extends GamePiece implements Moveable{
+	// leprechaun jumps around the map
+	private Boolean moveRight = true;
+	
+	public Leprechaun(char symbol, String label, int location) {
+		super('L', "Leprechaun", location);
+		
+	}
+	
+	public void draw() {
+		System.out.print(symbol);
+	}
+	
+	@Override
+	public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
+		if (this.getLocation()==playerLocation) {
+			return InteractionResult.ADVANCE;
+		} else {
+			return InteractionResult.NONE;
+		}
+		
+	}
+
+	@Override
+	public void move(Drawable[] gameBoard, int playerLocation) {
+		// can only be set to odd numbers in the level setup
+		if (moveRight && getLocation() == 19) {
+			moveRight = false;
+		} else if (!moveRight && getLocation() == 1) {
+			moveRight = true;
+		}
+		
+		
+		if ((moveRight) && (getLocation()+2 < 20) && (gameBoard[getLocation()+2] == null)) {
+			gameBoard[getLocation()] = null;
+			setLocation(getLocation()+2);
+			gameBoard[getLocation()] = this;
+		} else if ((!moveRight) && (getLocation()-2 > 0) && (gameBoard[getLocation()-2] == null)){
+			gameBoard[getLocation()] = null;
+			setLocation(getLocation()-1);
+			gameBoard[getLocation()] = this;
+		} else if((gameBoard[getLocation()+2] != null) &&  gameBoard[getLocation()+1] == null) {
+			gameBoard[getLocation()] = null;
+			setLocation(getLocation()+1);
+			gameBoard[getLocation()] = this;
+		} else if((gameBoard[getLocation()-2] != null) && gameBoard[getLocation()-1] == null) {
+			gameBoard[getLocation()] = null;
+			setLocation(getLocation()-2);
+			gameBoard[getLocation()] = this;
+		}
+		
+	}
+
+}
---
+++ src/levelPieces/Witch.java
@@ -0,0 +1,45 @@
+package levelPieces;
+import java.util.Random;
+import gameEngine.Drawable;
+import gameEngine.InteractionResult;
+import gameEngine.Moveable;
+
+public class Witch extends GamePiece implements Moveable{
+	
+	//distance of two away and is a hit
+	//doesn't matter if objects are in sight
+	
+	
+	public Witch(int location) {
+		super('W', "Witch", location);		
+	}
+	
+	
+
+	public void draw() {
+		System.out.print(symbol);
+	}
+	
+	
+	@Override
+	public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
+		int spellZone = Math.abs(playerLocation - getLocation());
+		if (spellZone < 3){
+			return InteractionResult.HIT;	// hits in range of 2
+		} else {
+			return InteractionResult.NONE;
+		}
+		
+	}
+	@Override
+	public void move(Drawable[] gameBoard, int playerLocation) {
+		Random rand = new Random();
+		int teleport = rand.nextInt(20);
+		
+		if (teleport < 20 && gameBoard[teleport] == null) {
+			gameBoard[getLocation()] = null;				// reset the board spot
+			setLocation(teleport);							
+			gameBoard[getLocation()] = this;				//new location
+		}
+	}
+}
